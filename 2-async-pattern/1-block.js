bb2722642963e74b95c7789cd89dc60a2c7841b5
Ran You <ran.you8311@gmail.com>
2021-03-13 17:05:13 -0800

    async patterns complete

---
+++ 2-async-pattern/1-block.js
@@ -0,0 +1,19 @@
+const http=require('http')
+
+const server=http.createServer((req,res)=>{
+if (req.url==='/'){
+    res.end('home page')
+} if (req.url==='/about'){
+    for (let i=0;i<1000;i++){
+        for (let j=0;j<1000;j++){
+          console.log(`${j}${i}`)  
+        }
+    }
+    res.end('About page')
+}
+})
+
+server.listen(5000,(
+)=>{
+    console.log('server is litening at 5000')
+})
